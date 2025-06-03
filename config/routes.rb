83e2d21df7caf2cfa05cc728e673f8d96481dbb8
Kei <natsuki.kei105@gmail.com>
2025-06-03 18:47:57 +0000

    変更を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'foo/bar'
+  get 'foo/baz'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'static_pages#home'
+  get  'static_pages/about'
+  get 'static_pages/contact', to: 'static_pages#contact'
 end
