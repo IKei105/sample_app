f638ca8b9b28b6467d9b3c33453def73fd044307
Kei <natsuki.kei105@gmail.com>
2025-06-14 16:50:10 +0000

    変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
+# app/controllers/sessions_controller.rb
+
 class SessionsController < ApplicationController
   def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in(user) # ← ここでログイン処理（セッションに保存）
-      redirect_to user_path(user) # ← ユーザーページへリダイレクト
+    if user&.authenticate(params[:session][:password]) # ← ここ変更
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,12 +1,22 @@
-// app/javascript/custom/menu.js
+// メニュー操作
 
+// トグルリスナーを追加してクリックをリッスンする
 document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
+  if (hamburger){
+    hamburger.addEventListener("click", function(event) {
+      event.preventDefault();
+      let menu = document.querySelector("#navbar-menu");
+      menu.classList.toggle("collapse");
+    });
+  }
+
   let account = document.querySelector("#account");
   if (account) {
     account.addEventListener("click", function(event) {
-      event.preventDefault(); // クリックしてもページ遷移しないようにする
+      event.preventDefault();
       let menu = document.querySelector("#dropdown-menu");
-      menu.classList.toggle("active"); // activeつけたり消したり
+      menu.classList.toggle("active");
     });
   }
 });
