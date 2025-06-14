7c31c54aa6e9be6d45178ac4f3f3b2a1d540edfe
Kei <natsuki.kei105@gmail.com>
2025-06-14 18:03:48 +0000

    変更を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 # app/controllers/sessions_controller.rb
 
 class SessionsController < ApplicationController
+  
+  include SessionsHelper
+  
   def new; end
 
   def create
@@ -15,5 +18,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,27 +1,26 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
 
-  test "login with valid information" do
-    post login_path, params: { session: { email:    @user.email,
+  test 'login with valid information' do
+    post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
   end
 
-  test "login with valid email/invalid password" do
+  test 'login with valid email/invalid password' do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email,
-                                          password: "invalid" } }
+                                          password: 'invalid' } }
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
@@ -29,6 +28,31 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  
+  test 'login with valid information followed by logout' do
+    # ログイン処理
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert is_logged_in?
 
+    # ユーザー詳細ページにリダイレクトされてるか
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    # ナビゲーションリンク確認（ログイン時）
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
+
+    # ログアウト実行
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+    follow_redirect!
+
+    # ナビゲーションリンク確認（ログアウト後）
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
+  end
 end
